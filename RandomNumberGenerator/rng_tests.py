f2f9eef760068164d2efd920e8963ad3a1c7970f
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-10-11 13:30:55 +0100

    Generate random numbers using random

--- RandomNumberGenerator/rng_tests.py
+++ RandomNumberGenerator/rng_tests.py
@@ -1,9 +1,23 @@
 import unittest
 
+import random
+
+def generate_random_number(lower, upper):
+    if lower == upper:
+        return lower
+
+    return random.randint(lower, upper)
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_returns_if_lower_equals_upper(self):
+        self.assertEqual(5, generate_random_number(5,5))
+
+    def test_returns_number_in_range(self):
+        l = 1
+        u = 100
+        ret = generate_random_number(l, u)
+        self.assertTrue(l < ret < u)
 
 
 if __name__ == '__main__':
