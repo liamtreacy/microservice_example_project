0ca6a0f980385ac8cc01f630ace296fd55251da8
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-10-11 13:34:44 +0100

    Move RNG to own file

---
+++ RandomNumberGenerator/rng.py
@@ -0,0 +1,5 @@
+import random
+
+
+def generate_random_number(lower, upper):
+    return random.randint(lower, upper)
--- RandomNumberGenerator/rng_tests.py
+++ RandomNumberGenerator/rng_tests.py
@@ -1,17 +1,11 @@
 import unittest
 
-import random
-
-def generate_random_number(lower, upper):
-    if lower == upper:
-        return lower
-
-    return random.randint(lower, upper)
+from rng import generate_random_number
 
 
 class MyTestCase(unittest.TestCase):
     def test_returns_if_lower_equals_upper(self):
-        self.assertEqual(5, generate_random_number(5,5))
+        self.assertEqual(5, generate_random_number(5, 5))
 
     def test_returns_number_in_range(self):
         l = 1
