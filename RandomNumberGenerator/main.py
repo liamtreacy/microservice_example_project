ec8221d6ca9dea7e0397048f430d2883c2ac0f98
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-10-12 11:07:30 +0100

    Moved the queue name to the dockerfile

--- RandomNumberGenerator/main.py
+++ RandomNumberGenerator/main.py
@@ -12,18 +12,19 @@ if __name__ == '__main__':
     lower = int(os.environ['LOWER_BOUND'])
     upper = int(os.environ['UPPER_BOUND'])
     loop_interval_secs = int(os.environ['DELAY_LOOP_SECONDS'])
+    publish_queue = os.environ['PUBLISH_QUEUE']
 
     connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='test_rng')
+    channel.queue_declare(queue=publish_queue)
 
     while True:
         rnd = generate_random_number(lower, upper)
         rnd = str(rnd)
 
         channel.basic_publish(exchange='',
-                          routing_key='test_rng',
+                          routing_key=publish_queue,
                           body=rnd)
         print(" [x] Sent " + rnd)
         time.sleep(loop_interval_secs)
