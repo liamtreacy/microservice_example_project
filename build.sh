bdb409446dd20f04925e1a28a6acc1423ec411a8
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-10-12 08:30:30 +0100

    Added docker file

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Running tests"
 RandomNumberGenerator/venv/bin/python RandomNumberGenerator/rng_tests.py
+
+echo "Building docker image"
+cd RandomNumberGenerator
+docker build --tag rng-docker .
