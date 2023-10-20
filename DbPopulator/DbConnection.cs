bf815a250e505e75552dad934867a7c3bf96f1da
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-10-20 10:43:12 +0100

    WIP

--- DbPopulator/DbConnection.cs
+++ DbPopulator/DbConnection.cs
@@ -3,10 +3,12 @@ public class DbConnection
     private string db;
     private string user;
     private string pass;
-    public DbConnection(string _db, string _user, string _pass)
+    private string collection_name;
+    public DbConnection(string _db, string _user, string _pass, string _collection_name)
     {
         db = _db;
         user = _user;
         pass = _pass;
+        collection_name = _collection_name;
     }
 }
--- DbPopulator/Program.cs
+++ DbPopulator/Program.cs
@@ -5,43 +5,33 @@ string default_msg_provider_host_name = "localhost";
 string default_db_host_name = "localhost";
 string default_queue_name = "random_numbers_queue";
 
-string msg_provider_host_name;
-string queue_name;
-string db_host_name;
+//GetEnvVarOrDefault("HOSTDB", default_db_host_name, out db_host_name);
 
-GetEnvVarOrDefault("HOSTMSG",default_msg_provider_host_name, out msg_provider_host_name);
-GetEnvVarOrDefault("QUEUE", default_queue_name, out queue_name);
-GetEnvVarOrDefault("HOSTDB", default_db_host_name, out db_host_name);
-
-string db;
-string user;
-string pass;
-
-GetEnvVarOrDefault("DBUSER", "", out db);
-GetEnvVarOrDefault("DBPASS", "", out user);
-GetEnvVarOrDefault("DB", "", out pass);
-
-var db_conn = new DbConnection(db, user, pass);
-
+var db_conn = new DbConnection(GetEnvVarOrDefault("DBUSER", ""),
+                    GetEnvVarOrDefault("DBPASS", ""), 
+                    GetEnvVarOrDefault("DB", ""), 
+                    GetEnvVarOrDefault("DBCOLLECTION", ""));
 
 // Establish rabbit connection
-var message_reader = new MessageReader(msg_provider_host_name, 
+var message_reader = new MessageReader(
+            GetEnvVarOrDefault("HOSTMSG",default_msg_provider_host_name), 
                             (string s) => {
                                 var cmd = new UpdateDbCommand(s);
                                 cmd.Run();
                             });
 
-message_reader.Listen(queue_name);
+message_reader.Listen(GetEnvVarOrDefault("QUEUE", default_queue_name));
 
 Console.ReadLine();
 
 
-void GetEnvVarOrDefault(in string envvar, in string default_val, out string s)
+string GetEnvVarOrDefault(in string envvar, in string default_val)
 {
-    s = Environment.GetEnvironmentVariable(envvar);
+    var s = Environment.GetEnvironmentVariable(envvar);
 
     if(s == null)
     {
         s = default_val;
     }
+    return s;
 }
