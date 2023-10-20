c9512e073c7f6c1ea68bb58329123f8eb7ee0999
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-10-20 10:23:07 +0100

    WIP

--- DbPopulator/Program.cs
+++ DbPopulator/Program.cs
@@ -3,27 +3,37 @@
 var p = new Printer();
 p.Print("starting up");
 
-string default_host_name = "localhost";
+string default_msg_provider_host_name = "localhost";
+string default_db_host_name = "localhost";
+string default_queue_name = "random_numbers_queue";
 
-var host_name = Environment.GetEnvironmentVariable("HOST");
+string msg_provider_host_name;
+string queue_name;
+string db_host_name;
 
-if(host_name == null)
-{
-    host_name = default_host_name;
-}
+GetEnvVarOrDefault("HOSTMSG",default_msg_provider_host_name, out msg_provider_host_name);
+GetEnvVarOrDefault("QUEUE", default_queue_name, out queue_name);
+GetEnvVarOrDefault("HOSTDB", default_db_host_name, out db_host_name);
 
-var queue_name = "random_numbers_queue";//Environment.GetEnvironmentVariable("QUEUE");
+UpdateDbCommand.User = "";
 
 // Establish rabbit connection
-
-var message_reader = new MessageReader(host_name, 
+var message_reader = new MessageReader(msg_provider_host_name, 
                             (string s) => {
                                 p.Print($"LAMBDA: {s}");
                             });
 
 message_reader.Listen(queue_name);
 
-p.Print("NEAR END");
-
 Console.ReadLine();
 
+
+void GetEnvVarOrDefault(in string envvar, in string default_val, out string s)
+{
+    s = Environment.GetEnvironmentVariable(envvar);
+
+    if(s == null)
+    {
+        s = default_val;
+    }
+}
---
+++ DbPopulator/UpdateDbCommand.cs
@@ -0,0 +1,12 @@
+public class UpdateDbCommand
+{
+    public static string Db;
+    public static string User;
+    public static string Password;
+
+    public UpdateDbCommand(string message)
+    {}
+
+    public void Run()
+    {}
+}
