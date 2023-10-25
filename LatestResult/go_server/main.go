f5e4165db8f7477e2d884a82a6d0d641f48741be
Liam Treacy <liam.treacy@opencastsoftware.com>
2023-10-25 15:06:44 +0100

    WIP. Not retreiving documents

--- LatestResult/go_server/main.go
+++ LatestResult/go_server/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 
-	//"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
@@ -13,16 +13,15 @@ import (
 
 const uri = "mongodb://my_reader:my_password@mongo:27017/?authSource=my_db"
 
-
+type LotteryResult struct {
+    Numbers []int
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, World!")
-    })
-
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	
 
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), opts)
@@ -37,6 +36,22 @@ func main() {
 	}()
 
 	fmt.Println("You successfully connected to MongoDB!")
+	collection := client.Database("my_db").Collection("lottery_collection")
+
+	//filter := bson.D{{"latest", "1, 2, 3"}}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        //fmt.Fprintf(w, "Hello, World!")
+		var result LotteryResult
+
+		err = collection.FindOne(context.TODO(), bson.D{}).Decode(&result)
+		if err != nil {
+			//log.
+			log.Fatal(err)
+		}
+
+		fmt.Fprintf(w, "Found a single document: %+v\n", result)
+    })
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
